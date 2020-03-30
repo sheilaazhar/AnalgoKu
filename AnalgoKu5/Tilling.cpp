2e8f15ed344b5b6ded630cad96951042dc9082ec
Sheila Azhar <sheila.azhar98@gmail.com>
2020-03-30 22:30:57 +0700

    AnalgoKu

---
+++ AnalgoKu5/Tilling.cpp
@@ -0,0 +1,49 @@
+/*
+Nama		: Sheila Azhar Almufarida
+NPM			: 140810180001
+Nama Program: Tilling Problem
+*/
+
+// C++ implementation to count number of ways to 
+// tile a floor of size n x m using 1 x m tiles 
+#include <bits/stdc++.h> 
+  
+using namespace std; 
+  
+// function to count the total number of ways 
+int countWays(int n, int m) 
+{ 
+  
+    // table to store values 
+    // of subproblems 
+    int count[n + 1]; 
+    count[0] = 0; 
+  
+    // Fill the table upto value n 
+    for (int i = 1; i <= n; i++) { 
+        // recurrence relation 
+        if (i > m) 
+            count[i] = count[i - 1] + count[i - m]; 
+  
+        // base cases 
+        else if (i < m) 
+            count[i] = 1; 
+  
+        // i = = m 
+        else
+            count[i] = 2; 
+    } 
+  
+    // required number of ways 
+    return count[n]; 
+} 
+  
+// Driver program to test above 
+int main() 
+{ 
+    int n = 4, m = 2; 
+    cout << "Number of ways = "
+         << countWays(n, m); 
+    return 0; 
+} 
+
